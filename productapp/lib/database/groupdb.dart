6ee7bbafcaf473cb2c3f4d08f9ac300dd2863e8c
Inkashi <zabolotin.nikitka@mail.ru>
2024-11-01 20:00:17 +0500

    -

--- productapp/lib/database/groupdb.dart
+++ productapp/lib/database/groupdb.dart
@@ -17,16 +17,4 @@ class Favorite {
   void updateData() {
     _myBox.put("groups", groups);
   }
-
-//Если создавать группы в которые можно будет добавлять продукты
-  // void createGroup(String groupName) {
-  //   groups.add({'name': groupName, 'items': []});
-  //   updateData();
-  // }
-
-  // void addToGroup(String name, dynamic item) {
-  //   final group = groups.firstWhere((e) => e['name'] == name);
-  //   group['items'].add(item);
-  //   updateData();
-  // }
 }
